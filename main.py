08a533e7d194669e651293b25e9336953f4dbf0c
R Hari Narayan <52242190+R-Hari-Narayan@users.noreply.github.com>
2022-02-04 20:42:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 inpt= open("a.txt","r")
 line1= inpt.readline()
 inpt_lst= line1.split()
-D= inpt_lst[0]  #D is Duration of simulation
-I= inpt_lst[1]  #I is the no. of intersections
-S= inpt_lst[2]  #S is the no. of streets
-V= inpt_lst[3]  #V is the no. of cars
-F= inpt_lst[4]  #F is the bonus point for each car that reaches its destination
+D= int(inpt_lst[0])  #D is Duration of simulation
+I= int(inpt_lst[1])  #I is the no. of intersections
+S= int(inpt_lst[2])  #S is the no. of streets
+V= int(inpt_lst[3])  #V is the no. of cars
+F= int(inpt_lst[4])  #F is the bonus point for each car that reaches its destination before time D
